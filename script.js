6d935b9f47598be50aa3d5eb135db182fd2fc4e1
Vinícius Levi <viniciuslevi152@gmail.com>
2020-09-17 19:44:15 -0300

    Segunda versão

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 /**
  * Algo de errado não está certo
  */
-var idade = window.prompt('Qual a sua idade?')
-window.alert(idade>=18?'Você é de maior':'Voce é de menor')
+
+ var ed = document.querySelector('#ed')
+ ed.addEventListener('click', ed_clicar)
+ var inf = document.querySelector('#inf')
+ inf.addEventListener('click', inf_clicar)
+var nut = document.querySelector('#nut')
+ nut.addEventListener('click', nut_clicar)
+ var seg = document.querySelector('#seg')
+ seg.addEventListener('click', seg_clicar)
+var cursos = document.querySelector('.cursos')
+
+ function ed_clicar(){
+
+ }
+ function inf_passar(){
+     inf.innerHTM="olá"
+ }
+ function nut_passar(){
+    nut.innerHTM="Tentando"
+}
+function seg_passar(){
+    seg.innerHTM="olá"
+}
