08705a3f24d4b2faccb8a636b832a9522ea0583d
Vinicius Levi <viniciuslevi152@gmail.com>
2021-09-09 20:14:14 -0300

    1° versão

--- script.js
+++ script.js
@@ -1,26 +1,54 @@
-/**
- * Algo de errado não está certo
- */
+let cursos = document.querySelector('.cursos')
+let cursos_opt = document.querySelector('.cursos_opt')
 
- var ed = document.querySelector('#ed')
- ed.addEventListener('click', ed_clicar)
- var inf = document.querySelector('#inf')
- inf.addEventListener('click', inf_clicar)
-var nut = document.querySelector('#nut')
- nut.addEventListener('click', nut_clicar)
- var seg = document.querySelector('#seg')
- seg.addEventListener('click', seg_clicar)
-var cursos = document.querySelector('.cursos')
+cursos.addEventListener('mouseover', () =>{
+    cursos_opt.style.display = "unset"; //if mouse be on cursos, cursos_opt'll be visible 
 
- function ed_clicar(){
+    cursos_opt.addEventListener('mouseover', () =>{
+        cursos_opt.style.display = "unset"; //now if mouse be on cursos_opt, 
+    })
 
- }
- function inf_passar(){
-     inf.innerHTM="olá"
- }
- function nut_passar(){
-    nut.innerHTM="Tentando"
+})
+cursos_opt.addEventListener('mouseout', () => {
+    cursos_opt.style.display = "none";
+    
+    cursos.addEventListener('mouseout', a => {
+        cursos_opt.style.display = "none"
+    })
+
+})
+//CALCULADORA
+
+var calc_display = document.querySelector('#calc_display')
+calc_display.addEventListener('click', clicou)
+
+function clicou(){
+    document.querySelector('.calculadora').style.display="unset"
 }
-function seg_passar(){
-    seg.innerHTM="olá"
+
+var nt1 = document.querySelector('#nt1')
+var nt2 = document.querySelector('#nt2')
+var nt3 = document.querySelector('#nt3')
+var nt4 = document.querySelector('#nt4')        
+var exibir = document.querySelector('#exibir')
+
+function media(){
+
+    var n1 = Number(nt1.value)
+    var n2 = Number(nt2.value)
+    var n3 = Number(nt3.value)
+    var n4 = Number(nt4.value)
+
+    var media
+    if(n1 ==0 || n2==0){
+        return ( exibir.innerHTML = `Impossivel calcular`)
+    }else if(n3 == 0 && n4 == 0){
+        media = (n1 + n2)/2 
+      }else if(n3 != 0 && n4 == 0){
+        media = (n1+n2+n3)/3
+    }else if(n3!=0 && n4 !=0){
+        media = (n1 + n2 + n3 + n4)/4
+    }
+    exibir.innerHTML = `A sua media é ${media}`        
+    
 }
